9636ae156568de6417d64f8723dd7aa3057c4351
llevttarr <llevttarr@gmail.com>
2025-11-09 11:28:46 +0200

    code to flash the pico LED

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
 #![no_std]
 #![no_main]
 
-fn main() {
+use embedded_hal::digital::v2::OutputPin;
+use panic_halt as _;
+
+use rp_pico::entry;
+use rp_pico::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    watchdog::Watchdog,
+    Sio,
+};
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let clocks = init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let sio = Sio::new(pac.SIO);
+    let pins = rp_pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+    let mut led_pin = pins.led.into_push_pull_output();
+    loop {
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+    }
 }
