ff029b10a88a2e6e888ae21f74f5ca1990e2a240
llevttarr <llevttarr@gmail.com>
2025-11-12 11:39:45 +0200

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
-use embedded_hal::prelude::_embedded_hal_serial_Read;
-use embedded_hal::digital::v2::OutputPin;
+
+use embedded_hal::serial::Read;
 use panic_halt as _;
 
 use rp_pico::entry;
@@ -11,13 +11,16 @@ use rp_pico::hal::{
     pac,
     uart::{DataBits, StopBits, UartConfig, UartPeripheral},
     watchdog::Watchdog,
-    Sio,
+    Sio
 };
 
+use usb_device::class_prelude::UsbBusAllocator;
+use usbd_serial::SerialPort;
+use usb_device::prelude::UsbVidPid;
+use usb_device::prelude::UsbDeviceBuilder;
 #[entry]
 fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let clocks = init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
@@ -26,41 +29,53 @@ fn main() -> ! {
         pac.PLL_SYS,
         pac.PLL_USB,
         &mut pac.RESETS,
-        &mut watchdog).ok().unwrap();
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
     let sio = Sio::new(pac.SIO);
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
     let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
-        &mut pac.RESETS);
+        &mut pac.RESETS,
+    );
+
+    let usb_bus = UsbBusAllocator::new(rp_pico::hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let mut serial = SerialPort::new(&usb_bus);
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .device_class(2)
+        .build();
+
     let mut led_pin = pins.led.into_push_pull_output();
-    // gps uses gpio8 and gpio9
     let uart_pins = (
         pins.gpio8.into_function::<rp_pico::hal::gpio::FunctionUart>(),
-        pins.gpio9.into_function::<rp_pico::hal::gpio::FunctionUart>(),
+        pins.gpio9.into_function::<rp_pico::hal::gpio::FunctionUart>()
     );
-    
     let mut uart = UartPeripheral::new(pac.UART1, uart_pins, &mut pac.RESETS)
         .enable(
             UartConfig::new(HertzU32::Hz(9600), DataBits::Eight, None, StopBits::One),
-            clocks.peripheral_clock.freq(),
+            clocks.peripheral_clock.freq()
         )
         .unwrap();
-    let mut gps_buffer: [u8;256] = [0;256];
-    let mut buffer_index = 0;
-    led_pin.set_high().unwrap();
-    delay.delay_ms(500);
-    led_pin.set_low().unwrap();
-    delay.delay_ms(500);
     loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
-        while let Ok(byte) = uart.read() {
-            gps_buffer[buffer_index] = byte;
-            buffer_index = (buffer_index+1) % gps_buffer.len();
+        if usb_dev.poll(&mut [&mut serial]) {
+            match uart.read() {
+                Ok(b) => {
+                    let _ = serial.write(&[b]);
+                }
+                Err(_) => {}
+            }
         }
     }
 }
